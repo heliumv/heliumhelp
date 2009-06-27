693de2537b6dc34cdb99887d2dfa28083f243ffc
werner <werner>
2009-06-27 11:55:01 +0000

    *** empty log message ***

--- jsp/jhelpbuilder/jhelpbuilder.bat
+++ jsp/jhelpbuilder/jhelpbuilder.bat
@@ -1,3 +1,5 @@
+rem muss aus dem Verzeichnis des jhelbuilder gestartet werden
 @echo on;
-cd C:\jbproject\heliumhelp\jhelpbuilder
-java -jar C:\jbproject\heliumhelp\jhelpbuilder\lib\cjh.jar 1>stdout.log 2>stderr.log
+rem old cd C:\jbproject\heliumhelp\jhelpbuilder
+rem old java -jar C:\jbproject\heliumhelp\jhelpbuilder\lib\cjh.jar 1>stdout.log 2>stderr.log
+java -jar lib\cjh.jar 1>stdout.log 2>stderr.log
--- jsp/jhelpbuilder/preview_onlinehelp.bat
+++ jsp/jhelpbuilder/preview_onlinehelp.bat
@@ -1,3 +1,4 @@
+rem muss aus dem Verzeichnis des jhelbuilder gestartet werden
 @echo on
 
 java -jar lib/hsviewer.jar -helpset ../heliumhelp/HeliumOnlineHelp.hs
