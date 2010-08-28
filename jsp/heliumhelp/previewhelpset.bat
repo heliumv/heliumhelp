75f30ba97840cdfadeab1bd211d14b18e4424a08
werner <werner>
2010-08-28 12:37:21 +0000

    *** empty log message ***

--- jsp/heliumhelp/previewhelpset.bat
+++ jsp/heliumhelp/previewhelpset.bat
@@ -3,3 +3,5 @@
 @echo on
 
 java -jar C:\javahelp-2_0_02\jh2.0\demos\bin\hsviewer.jar -helpset C:\jbproject\lpclientpc\heliumv_help\HeliumOnlineHelp.hs -classpath .;C:\jbproject\lpclientpc\heliumv_help
+
+pause
