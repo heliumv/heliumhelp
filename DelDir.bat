1a07642e3208e193f619184730512fd02dafa1b8
werner <werner>
2010-11-26 16:01:34 +0000

    *** empty log message ***

---
+++ DelDir.bat
@@ -0,0 +1,101 @@
+cd Allgemein
+RMDIR %1 /S /Q
+cd ..
+cd Allgemeine_Stammdaten
+RMDIR %1 /S /Q
+cd ..
+cd Anfrage
+RMDIR %1 /S /Q
+cd ..
+cd Angebot
+RMDIR %1 /S /Q
+cd ..
+cd Angebotstueckliste
+RMDIR %1 /S /Q
+cd ..
+cd artikel
+RMDIR %1 /S /Q
+cd ..
+cd auftrag
+RMDIR %1 /S /Q
+cd ..
+cd Begriffe
+RMDIR %1 /S /Q
+cd ..
+cd benutzer
+RMDIR %1 /S /Q
+cd ..
+cd Bestellung
+RMDIR %1 /S /Q
+cd ..
+cd Bilder
+RMDIR %1 /S /Q
+cd ..
+cd Client
+RMDIR %1 /S /Q
+cd ..
+cd Eingangsrechnung
+RMDIR %1 /S /Q
+cd ..
+cd Erste_Schritte
+RMDIR %1 /S /Q
+cd ..
+cd Fertigung
+RMDIR %1 /S /Q
+cd ..
+cd Finanzbuchhaltung
+RMDIR %1 /S /Q
+cd ..
+cd images
+RMDIR %1 /S /Q
+cd ..
+cd Installation
+RMDIR %1 /S /Q
+cd ..
+cd Kueche
+RMDIR %1 /S /Q
+cd ..
+cd Kunde
+RMDIR %1 /S /Q
+cd ..
+cd Lieferant
+RMDIR %1 /S /Q
+cd ..
+cd Lieferschein
+RMDIR %1 /S /Q
+cd ..
+cd Partner
+RMDIR %1 /S /Q
+cd ..
+cd personal
+RMDIR %1 /S /Q
+cd ..
+cd Projekte
+RMDIR %1 /S /Q
+cd ..
+cd Rechnung
+RMDIR %1 /S /Q
+cd ..
+cd Reklamation
+RMDIR %1 /S /Q
+cd ..
+cd searchDB
+RMDIR %1 /S /Q
+cd ..
+cd Statusdiagramme
+RMDIR %1 /S /Q
+cd ..
+cd Stueckliste
+RMDIR %1 /S /Q
+cd ..
+cd System
+RMDIR %1 /S /Q
+cd ..
+cd Zeiterfassung
+RMDIR %1 /S /Q
+cd ..
+cd Zutritt
+RMDIR %1 /S /Q
+cd ..
+
+pause
