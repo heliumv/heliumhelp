57bbebd0c00776124a9748d277b93672bbe40a15
werner <werner@HeliumV.com>
2013-03-15 08:23:42 +0100

    DB-Index geht anscheinend wieder

--- Make_a_local_copy.bat
+++ Make_a_local_copy.bat
@@ -8,7 +8,7 @@ rem die Pfade sind fix festgelegt
 
 echo "kopieren des Source"
 d:
-cd \CVS_HV\heliumhelp\jsp\
+cd \GIT_HV\heliumhelp\jsp\
 c:
 cd \jbproject\heliumhelp\jsp\
 xcopy d:*.* /v/s/y
@@ -17,11 +17,11 @@ pause
 echo "Entfernen aller nicht benötigten Dateien und Ordner"
 c:
 cd \jbproject\heliumhelp\jsp\heliumhelp\
-call d:\CVS_HV\heliumhelp\DelDir.bat _vti_cnf\
-call d:\CVS_HV\heliumhelp\DelDir.bat _vti_pvt\
-call d:\CVS_HV\heliumhelp\DelDir.bat CVS\
-call d:\CVS_HV\heliumhelp\DelDir.bat _derived\
-call d:\CVS_HV\heliumhelp\DelDir.bat backup\
+call d:\GIT_HV\heliumhelp\DelDir.bat _vti_cnf\
+call d:\GIT_HV\heliumhelp\DelDir.bat _vti_pvt\
+call d:\GIT_HV\heliumhelp\DelDir.bat CVS\
+call d:\GIT_HV\heliumhelp\DelDir.bat _derived\
+call d:\GIT_HV\heliumhelp\DelDir.bat backup\
 del .cvsignore /s
 del Thumbs.db /s
 del *.bak /s
